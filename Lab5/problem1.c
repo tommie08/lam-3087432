7eb595d613f8a0aa89780f2d7c882f92588a7dbc
tommie08 <tommylam59@gmail.com>
2023-10-15 21:47:12 -0500

    Completed Lab

--- Lab5/problem1.c
+++ Lab5/problem1.c
@@ -1,68 +1,107 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    int num[12];
-    File* stats = fopen("input.txt", "r");
+#define MONTHS 12
 
-    if (!stats){
-        printf("Error opening the file...\n");
-        return -1;
+
+void printMonthlyReport(double sales[]);
+void printSalesSummary(double sales[]);
+void printMovingAverages(double sales[]);
+void printSalesHighToLow(double sales[]);
+
+const char *months[MONTHS] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+const char* getMonthName(int index) {
+    return months[index];
+}
+
+int main() {
+    FILE *file = fopen("input.txt", "r");
+    if (file == NULL) {
+        printf("Error opening input file!\n");
+        return 1;
     }
 
-    for (int i = 0; i < 12; ++i) {
-        if (fscanf(stats, "d", &num[i]) != 1) {
-            printf("File contains invalid line\n");
-            return -1;
-        }
-        num[i] = 
+    double sales[MONTHS];
+    for (int i = 0; i < MONTHS; i++) {
+        fscanf(file, "%lf", &sales[i]);
     }
+    fclose(file);
 
-    fclose(stats);
-    return 0
-
-    printf("Monthly sales report for 2022:\n");
-    printf("\n Month        Sales\n");
-    printf("\nJanuary    $");
-    printf("\nFebruary   $");
-    printf("\nMarch      $");
-    printf("\nApril      $");
-    printf("\nMay        $");
-    printf("\nJune       $");
-    printf("\nJuly       $");
-    printf("\nAugust     $");
-    printf("\nSeptember  $");
-    printf("\nOctober    $");
-    printf("\nNovember   $");
-    printf("\nDecember   $\n");
-    
-    printf("\nSales summary:\n");
-    printf("\nMinimun sales:   $");
-    printf("\nMaximun sales:   $");
-    printf("\nAverage sales:   $\n");
-    
-    printf("\nSix-Month Moving Average Report:\n");
-    printf("\nJanuary    -  June       $");
-    printf("\nFebruary   -  July       $");
-    printf("\nMarch      -  August     $");
-    printf("\nApril      -  September  $");
-    printf("\nMay        -  October    $");
-    printf("\nJune       -  November   $");
-    printf("\nJuly       -  December   $\n");
-    
-    printf("\nSales Report (Highest to Lowest:\n");
-    printf("\n Month        Sales\n");
-    printf("\nJanuary    $");
-    printf("\nFebruary   $");
-    printf("\nMarch      $");
-    printf("\nApril      $");
-    printf("\nMay        $");
-    printf("\nJune       $");
-    printf("\nJuly       $");
-    printf("\nAugust     $");
-    printf("\nSeptember  $");
-    printf("\nOctober    $");
-    printf("\nNovember   $");
-    printf("\nDecember   $\n");
+    printMonthlyReport(sales);
+    printSalesSummary(sales);
+    printMovingAverages(sales);
+    printSalesHighToLow(sales);
     return 0;
+}
+
+void printMonthlyReport(double sales[]) {
+    printf("Monthly sales report for 2022:\n");
+    printf("Month Sales\n");
+    for (int i = 0; i < MONTHS; i++) {
+        printf("%-10s $%.2lf\n", months[i], sales[i]);
+    }
+    printf("\n");
+}
+
+void printSalesSummary(double sales[]) {
+    double min = sales[0], max = sales[0], total = 0.0;
+    int minMonth = 0, maxMonth = 0;
+    for (int i = 0; i < MONTHS; i++) {
+        if (sales[i] < min) {
+            min = sales[i];
+            minMonth = i;
+        }
+        if (sales[i] > max) {
+            max = sales[i];
+            maxMonth = i;
+        }
+        total += sales[i];
+    }
+    printf("Sales summary:\n");
+    printf("Minimum sales: $%.2lf (%s)\n", min, getMonthName(minMonth));
+    printf("Maximum sales: $%.2lf (%s)\n", max, getMonthName(maxMonth));
+    printf("Average sales: $%.2lf\n\n", total / MONTHS);
+}
+
+void printMovingAverages(double sales[]) {
+    printf("Six-Month Moving Average Report:\n");
+    for (int i = 0; i < MONTHS - 5; i++) {
+        double sum = 0.0;
+        for (int j = i; j < i + 6; j++) {
+            sum += sales[j];
+        }
+        printf("%s - %s $%.2lf\n", getMonthName(i), getMonthName(i+5), sum / 6.0);
+    }
+    printf("\n");
+}
+
+void printSalesHighToLow(double sales[]) {
+    typedef struct {
+        double value;
+        int index;
+    } SaleData;
+
+    SaleData sortedSales[MONTHS];
+    for (int i = 0; i < MONTHS; i++) {
+        sortedSales[i].value = sales[i];
+        sortedSales[i].index = i;
+    }
+
+    for (int i = 0; i < MONTHS - 1; i++) {
+        for (int j = 0; j < MONTHS - i - 1; j++) {
+            if (sortedSales[j].value < sortedSales[j + 1].value) {
+                SaleData temp = sortedSales[j];
+                sortedSales[j] = sortedSales[j + 1];
+                sortedSales[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sales Report (Highest to Lowest):\n");
+    printf("Month Sales\n");
+    for (int i = 0; i < MONTHS; i++) {
+        printf("%-10s $%.2lf\n", getMonthName(sortedSales[i].index), sortedSales[i].value);
+    }
+    printf("\n");
 }
