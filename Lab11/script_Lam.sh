ccc7ed8d3d90dec03647d654af2b851112e1612f
Tommy Lam <93234446+tommie08@users.noreply.github.com>
2023-12-01 17:29:23 -0600

    Update script_Lam.sh

--- Lab11/script_Lam.sh
+++ Lab11/script_Lam.sh
@@ -8,19 +8,19 @@ echo "$input" | grep -o 'bo.*ng'
 
 # Question 2: In the input string, Match a word, at least four letters long, that doesn’t contain "u"
 echo "Question2"
-echo "$input" | grep -o '\b(?:[^u\W]{4,})\b'
+echo "$input" | grep -oP '\b(?:[^u\W]{4,})\b'
 
 # Question 3: In the input string, match three consecutive words, where the middle word begins with "b".
 echo "Question3"
-echo "$input" | grep -o '\b\w+\b \bb\w+\b \b\w+\b'
+echo "$input" | grep -oP '\b\w+\b \bb\w+\b \b\w+\b'
 
 # Question 4: Identify all function declarations (including parameters)
 echo "Question4"
-#grep -E 'regex_goes_here' code.c
+grep -E 'regex_goes_here' code.c
 
 # Question 5: Extract lines where a C++ structure is defined using awk
 echo "Question5"
-#awk 'regex_goes_here' code.c
+awk 'regex_goes_here' code.c
 
 # Question 6: Replace "marks" with "score" in the code1_2.cpp using sed
 echo "Question6"
